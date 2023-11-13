b7c430f41c6748cd6192570604272d5f00460877
valve01 <118441831+valve01@users.noreply.github.com>
2023-11-13 23:24:41 +0300

    г

--- src/js/modules/mini-img-html-render.js
+++ src/js/modules/mini-img-html-render.js
@@ -8,3 +8,17 @@
 // 	miniImgContainer.insertAdjacentElement('beforeend', el);
 // };
 // export default miniImgHtmlRender;
+
+	// ========================Отображаем иконки слайдов ===================================================
+	let htmldecrSlide = "";
+	let decreasedSlides = document.createElement("div");
+	decreasedSlides.className = "decreasedSlides";
+	const apartmentSliderWrapper = document.querySelectorAll(
+		".apartmentSliderWrapper"
+	);
+	decrSlide.forEach((slide) => {
+		htmldecrSlide += `<div class="decreasedSlide"><img src="${slide.src}" onload = "this.closest('.decreasedSlide').style.display = 'flex'" onerror = "this.closest('.decreasedSlide').remove()"></div>
+		`;
+		decreasedSlides.innerHTML = htmldecrSlide;
+		apartmentSliderWrapper[i].appendChild(decreasedSlides);
+	});

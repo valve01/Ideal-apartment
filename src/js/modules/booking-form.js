559ac091b17513671625466bd7edfbd63573b6f5
valve01 <118441831+valve01@users.noreply.github.com>
2023-11-24 17:15:50 +0300

    модалка - форма

--- src/js/modules/booking-form.js
+++ src/js/modules/booking-form.js
@@ -2,6 +2,8 @@ const asideBookingFormEl = document.querySelector('.aside-booking-form');
 const showBookingFormBtnEl = document.querySelector('.show-booking-form-btn');
 const bookingFormEl = document.querySelector('.booking-form');
 const bodyEl = document.querySelector('.body');
+const bookingFormContainer = document.querySelector('.booking-form__booking-form-container');
+console.log(bookingFormContainer);
 
 const showForm = (e) => {
 	e.stopPropagation();
@@ -10,9 +12,7 @@ const showForm = (e) => {
 	bookingFormEl.classList.add('shading');
 	showBookingFormBtnEl.classList.add('none');
 };
-const closeForm = (e) => {
-	e.preventDefault();
-	e.stopPropagation();
+const closeForm = () => {
 	asideBookingFormEl.classList.remove('aside-booking-form--show');
 	bookingFormEl.classList.remove('booking-form--open');
 	bookingFormEl.classList.remove('shading');
@@ -20,5 +20,5 @@ const closeForm = (e) => {
 };
 
 showBookingFormBtnEl.addEventListener('click', (e) => showForm(e));
-
-bodyEl.addEventListener('click', (e) => closeForm(e));
+bookingFormContainer.addEventListener('click', (e) => e.stopPropagation());
+bodyEl.addEventListener('click', () => closeForm());

1b93db2ce630423d871e7fccdc7a17719b47b01b
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-04-01 23:12:02 +0300

    d

--- src/js/modules/rules-redirect.js
+++ src/js/modules/rules-redirect.js
@@ -1,8 +1,17 @@
 const rulesLink = $(
 	".header__menu-link[data-cat='rules'], .footer__item-text-link[data-cat='rules']",
 );
-rulesLink.on('click', function (e) {
-	const rulesBlock = document.querySelector('.rules-block');
+const rulesBlock = document.querySelector('.rules-block');
+const scrollToRules = () => {
 	rulesBlock.scrollIntoView(top);
+};
+
+rulesLink.on('click', function (e) {
 	e.preventDefault();
+	scrollToRules();
 });
+
+if (document.referrer.includes('apartment')) {
+	// console.log(' содержит apartment');
+	scrollToRules();
+}

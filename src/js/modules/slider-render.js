059ef1e55cfed0b99becddc7eb758a6f4b5e52ba
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-06-28 09:45:18 +0300

    сделал инициализацию swiper пока на setTimeout, зато не багует, переделаю на промисы

--- src/js/modules/slider-render.js
+++ src/js/modules/slider-render.js
@@ -16,16 +16,61 @@ const apartmentSliderRender = (findApartment) => {
 		<div class="swiper-button-next"></div>
 	`;
 
+	// Рендерим минислайды
+	for (let i = 0; i < 40; i++) {
+		const miniImgUrlAvif = `${photosVar}/(${i + 1}).avif`;
+		const miniImgUrlWebp = `${photosVar}/(${i + 1}).webp`;
+		const miniImgUrlJpg = `${photosVar}/(${i + 1}).jpg`;
+		const miniImgLoad = new Image();
+		if (miniImgUrlAvif) {
+			miniImgLoad.src = miniImgUrlAvif;
+		} else if (miniImgUrlWebp) {
+			miniImgLoad.src = miniImgUrlWebp;
+		} else if (miniImgUrlJpg) {
+			miniImgLoad.src = miniImgUrlJpg;
+		}
+		// if (miniImgUrlJpg) {
+		// 	miniImgLoad.src = miniImgUrlJpg;
+		// }
+		miniImgLoad.onerror = () => {
+			miniImgLoad.remove();
+			// if (i == 39) {
+				// apartmentSliderSwiper();
+				// return true;
+			// }
+		};
+		miniImgLoad.onload = () => {
+			const thumbSlideEl = document.createElement('div');
+			thumbSlideEl.style.backgroundImage =
+				// `
+				// url("${miniImgUrlJpg}")
+				// `;
+				`
+				image-set(
+					url("${miniImgUrlAvif}") type("image/avif"),
+					url("${miniImgUrlWebp}") type("image/webp"),
+					url("${miniImgUrlJpg}") type("image/jpeg")
+				)
+			`;
+			thumbSlideEl.setAttribute('loading', 'lazy');
+			thumbSlideEl.classList.add('swiper-slide', 'slider-mini-img');
+			thumbsSliderWrapper.append(thumbSlideEl);
+			// if (i == 39) {
+				// apartmentSliderSwiper();
+				// return true;
+			// }
+		};
+	}
 	// Рендерим главный слайдер
 	for (let i = 0; i < 40; i++) {
 		const mainImgUrlAvif = `${photosVar}/(${i + 1}).avif`;
 		const mainImgUrlWebp = `${photosVar}/(${i + 1}).webp`;
 		const mainImgUrlJpg = `${photosVar}/(${i + 1}).jpg`;
-		const initMainSwiper = () => {
-			mainSlider.insertAdjacentHTML('beforeend', navigationSwiperHtml);
-			apartmentSliderSwiper();
-			baguetteBox.run('.swiper .swiper-wrapper');
-		};
+		// const initMainSwiper = () => {
+		// 	mainSlider.insertAdjacentHTML('beforeend', navigationSwiperHtml);
+		// 	apartmentSliderSwiper();
+		// 	baguetteBox.run('.swiper .swiper-wrapper');
+		// };
 		const mainImgLoad = new Image();
 		if (mainImgUrlAvif) {
 			mainImgLoad.src = mainImgUrlAvif;
@@ -40,7 +85,11 @@ const apartmentSliderRender = (findApartment) => {
 		mainImgLoad.onerror = () => {
 			mainImgLoad.remove();
 			if (i == 39) {
-				initMainSwiper();
+				mainSlider.insertAdjacentHTML('beforeend', navigationSwiperHtml);
+				// apartmentSliderSwiper();
+				baguetteBox.run('.swiper .swiper-wrapper');
+				// initMainSwiper();
+				// return true;
 			}
 		};
 		mainImgLoad.onload = () => {
@@ -64,54 +113,17 @@ const apartmentSliderRender = (findApartment) => {
 
 			mainSliderWrapper.insertAdjacentHTML('beforeend', mainSlideEl);
 			if (i == 39) {
-				initMainSwiper();
+				mainSlider.insertAdjacentHTML('beforeend', navigationSwiperHtml);
+				// apartmentSliderSwiper();
+				baguetteBox.run('.swiper .swiper-wrapper');
+				// initMainSwiper();
+				// return true;
 			}
 		};
 	}
-
-	// Рендерим минислайды
-	for (let i = 0; i < 40; i++) {
-		const miniImgUrlAvif = `${photosVar}/(${i + 1}).avif`;
-		const miniImgUrlWebp = `${photosVar}/(${i + 1}).webp`;
-		const miniImgUrlJpg = `${photosVar}/(${i + 1}).jpg`;
-		const miniImgLoad = new Image();
-		if (miniImgUrlAvif) {
-			miniImgLoad.src = miniImgUrlAvif;
-		} else if (miniImgUrlWebp) {
-			miniImgLoad.src = miniImgUrlWebp;
-		} else if (miniImgUrlJpg) {
-			miniImgLoad.src = miniImgUrlJpg;
-		}
-		// if (miniImgUrlJpg) {
-		// 	miniImgLoad.src = miniImgUrlJpg;
-		// }
-		miniImgLoad.onerror = () => {
-			miniImgLoad.remove();
-			if (i == 39) {
-				apartmentSliderSwiper();
-			}
-		};
-		miniImgLoad.onload = () => {
-			const thumbSlideEl = document.createElement('div');
-			thumbSlideEl.style.backgroundImage =
-				// `
-				// url("${miniImgUrlJpg}")
-				// `;
-				`
-				image-set(
-					url("${miniImgUrlAvif}") type("image/avif"),
-					url("${miniImgUrlWebp}") type("image/webp"),
-					url("${miniImgUrlJpg}") type("image/jpeg")
-				)
-			`;
-			thumbSlideEl.setAttribute('loading', 'lazy');
-			thumbSlideEl.classList.add('swiper-slide', 'slider-mini-img');
-			thumbsSliderWrapper.append(thumbSlideEl);
-			if (i == 39) {
-				apartmentSliderSwiper();
-			}
-		};
-	}
+	setTimeout(() => {
+		apartmentSliderSwiper();
+	}, 2000);
 };
 
 export default apartmentSliderRender;

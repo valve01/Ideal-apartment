7e0cc01f824145d4815ff76feaa0323a4bfe0041
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-07-02 18:16:43 +0300

    получил ответ от Promise

--- src/js/modules/slider-render.js
+++ src/js/modules/slider-render.js
@@ -4,7 +4,7 @@ const apartmentSliderRender = (findApartment) => {
 	const { photos } = findApartment;
 
 	// Проверка, если url на github, то вернуть /Ideal-apartment/${photos}, иначе ${photos}
-	let photosVar='';
+	let photosVar = '';
 	const setPhotosVar = () => {
 		if (window.location.href.includes('github')) {
 			photosVar = `/Ideal-apartment/${photos}`;
@@ -24,8 +24,6 @@ const apartmentSliderRender = (findApartment) => {
 	`;
 	const insertHtml = () => {
 		mainSlider.insertAdjacentHTML('beforeend', navigationSwiperHtml);
-		mainSliderWrapper.insertAdjacentHTML('beforeend', mainSliderItems);
-		thumbsSliderWrapper.insertAdjacentHTML('beforeend', thumbSliderItems);
 	};
 	const initSwiper = () => {
 		apartmentSliderSwiper();
@@ -35,56 +33,66 @@ const apartmentSliderRender = (findApartment) => {
 	let mainSliderItems = '';
 	let thumbSliderItems = '';
 
-	for (let i = 0; i < 40; i++) {
-		const mainImgUrlAvif = `${photosVar}/(${i + 1}).avif`;
-		const mainImgUrlWebp = `${photosVar}/(${i + 1}).webp`;
-		const mainImgUrlJpg = `${photosVar}/(${i + 1}).jpg`;
+	new Promise(function (resolve, rej) {
+		for (let i = 0; i < 40; i++) {
+			const mainImgUrlAvif = `${photosVar}/(${i + 1}).avif`;
+			const mainImgUrlWebp = `${photosVar}/(${i + 1}).webp`;
+			const mainImgUrlJpg = `${photosVar}/(${i + 1}).jpg`;
 
-		const mainImgLoad = new Image();
-		if (mainImgUrlAvif) {
-			mainImgLoad.src = mainImgUrlAvif;
-		} else if (mainImgUrlWebp) {
-			mainImgLoad.src = mainImgUrlWebp;
-		} else if (mainImgUrlJpg) {
-			mainImgLoad.src = mainImgUrlJpg;
+			const mainImgLoad = new Image();
+			if (mainImgUrlAvif) {
+				mainImgLoad.src = mainImgUrlAvif;
+			} else if (mainImgUrlWebp) {
+				mainImgLoad.src = mainImgUrlWebp;
+			} else if (mainImgUrlJpg) {
+				mainImgLoad.src = mainImgUrlJpg;
+			}
+
+			mainImgLoad.onload = () => {
+				const mainSlideEl = `
+							<a  href="${mainImgUrlWebp}" loading="lazy" class="swiper-slide slider-main-img" style="background-image:
+								image-set(
+									url('${mainImgUrlAvif}') type('image/avif'),
+									url('${mainImgUrlWebp}') type('image/webp'),
+									url('${mainImgUrlJpg}') type('image/jpeg')
+								)
+							"></a>
+						`;
+
+				const thumbSlideEl = `
+							<div loading="lazy" class="swiper-slide slider-mini-img" style="background-image: 
+								image-set(
+									url('${mainImgUrlAvif}') type('image/avif'),
+									url('${mainImgUrlWebp}') type('image/webp'),
+									url('${mainImgUrlJpg}') type('image/jpeg')
+								);">
+							</div>
+						`;
+				mainSliderItems += mainSlideEl;
+				thumbSliderItems += thumbSlideEl;
+
+				if (i == 39) {
+					resolve(mainSliderItems);
+				}
+			};
+			mainImgLoad.onerror = () => {
+				mainImgLoad.remove();
+				if (i == 39) {
+					resolve(mainSliderItems);
+				}
+			};
 		}
+	}).then(function (response) {
+		console.log('хуй');
+		console.log(response);
+		// if (i == 39) {
+		mainSliderWrapper.insertAdjacentHTML('beforeend', response);
+		insertHtml();
+		initSwiper();
 
-		mainImgLoad.onload = () => {
-
-			const mainSlideEl = `
-					<a  href="${mainImgUrlWebp}" loading="lazy" class="swiper-slide slider-main-img" style="background-image:
-						image-set(
-							url('${mainImgUrlAvif}') type('image/avif'),
-							url('${mainImgUrlWebp}') type('image/webp'),
-							url('${mainImgUrlJpg}') type('image/jpeg')
-						)
-					"></a>
-				`;
-
-			const thumbSlideEl = `
-					<div loading="lazy" class="swiper-slide slider-mini-img" style="background-image: 
-						image-set(
-							url('${mainImgUrlAvif}') type('image/avif'),
-							url('${mainImgUrlWebp}') type('image/webp'),
-							url('${mainImgUrlJpg}') type('image/jpeg')
-						);">
-					</div>
-				`;
-			mainSliderItems += mainSlideEl;
-			thumbSliderItems += thumbSlideEl;
-			if (i == 39) {
-				insertHtml();
-				initSwiper();
-			}
-		};
-		mainImgLoad.onerror = () => {
-			if (i == 39) {
-				insertHtml();
-				initSwiper();
-			}
-			mainImgLoad.remove();
-		};
-	}
+		// thumbsSliderWrapper.insertAdjacentHTML('beforeend', thumbSliderItems);
+		// }
+	});
 };
 
 export default apartmentSliderRender;
